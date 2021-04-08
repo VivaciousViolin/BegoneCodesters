7ee9d4e7aa36ae6abe223aefd8cf6a2c8560c0ed
Alex <81312945+VivaciousViolin@users.noreply.github.com>
2021-04-08 10:21:15 -0400

    Update Begonemath.py

--- Begonemath.py
+++ Begonemath.py
@@ -7,37 +7,45 @@ from selenium.webdriver.support.ui import WebDriverWait as wait #downloading and
 import time #slow down time
 import re #search through strings
 
-"""
+
 #relative file paths
 userprofile = os.environ['USERPROFILE']
 driverpath = os.path.join(userprofile, 'Documents', 'GitHub', 'ExtraCodingThings', 'chromedriver_win32', 'chromedriver.exe')
-
-
-url = "https://www.codesters.com/code/"
-
+print(driverpath)
+url = "https://www.4devs.com.br/gerador_de_cpf"
 
 def open_browser():
+    """
     driver = webdriver.Chrome(driverpath)
-    driver.get(url)
+    driver.get("https://www.codesters.com/code/")
     driver.find_element_by_id('top-nav-login-button').click()
     driver.get("https://www.codesters.com/login/clever/?next=/social-signup/")
     clever_ref_url = driver.current_url
     m = re.match('redirect_uri=(.*?)', "ref").group()
+    print(clever_ref_url)
     #re.match("(.*?):",string).group()
     if m:
         found = m.group(1)
     print(m)
+    print(found)
     time.sleep(5)
     driver.get("https://clever.com/oauth/authorize?state=vjqCqVqqEpUkC6QYauqVBdwfg4a607j0&redirect_uri=")
     time.sleep(30)
-    #driver.find_element_by_id('bt_gerar_cpf').click()
-    #text_field = driver.find_element_by_id('texto_cpf')
-    #text = wait(driver, 10).until(lambda driver: not text_field.text == 'Gerando...' and text_field.text)
-    #return text
+    """
+    driver = webdriver.Chrome(driverpath)
+    time.sleep(3)
+    print(url)
+    driver.get(url)
+    driver.find_element_by_id('bt_gerar_cpf').click()
+    text_field = driver.find_element_by_id('texto_cpf')
+    text = wait(driver, 10).until(lambda driver: not text_field.text == 'Gerando...' and text_field.text)
+    return text
+
+#open_browser()
 
 print(open_browser())
 
-"""
+
 #logic in the code
 one = 10
 two = 8
